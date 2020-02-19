822956c7844b4d4655bfce05f5c3ae3f52d512ac
Daniel Sasser <danielbsasser@gmail.com>
2020-02-19 04:01:57 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  attr_accessor :name, :artist 
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+    splitname = filename.split(" - ")
+    song = self.new(splitname[1])
+    artist = Artist.find_or_create_by_name(splitname[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
+  end
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+  
+end
